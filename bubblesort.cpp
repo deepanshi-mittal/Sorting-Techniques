ca05c9234838b1ca6ec17848e3a7f0f4210168bb
Deepanshi Mittal <42512006+deepu987@users.noreply.github.com>
2019-05-02 10:31:38 +0530

    c++ code of bubble sort
    Time complexity of this code is O(n^2).

---
+++ bubblesort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;  //n is size of array provided by user
+	int a[n];	//array a
+	for(int i=0;i<n;i++)	//to take input in array
+		cin>>a[i];
+	//bubble sort 
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				std::swap(a[j],a[j+1]);
+			}
+		}
+	}	//bubble sort ends
+	for(int i=0;i<n;i++)
+		cout<<"\n"<<a[i];	//printing sorted array
+}
